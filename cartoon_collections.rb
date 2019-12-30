517c107de172ad9558f5552ec085d55658bdb78b
Miles Higbie <miles.higbie@gmail.com>
2019-12-30 22:43:06 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,6 +8,6 @@ end
 
 def summon_captain_planet(array) 
   array.map do |element|
-    "#{element[0].upcase}#{element.slice(1,..)}!"
+    "#{element[0].upcase}#{element.slice(1,element.last)}!"
   end 
 end 
